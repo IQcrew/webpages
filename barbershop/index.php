eef6be6eddea03f9ffec6d8051672d82333c5d94
TobiasMikula <94800323+IQcrew@users.noreply.github.com>
2024-02-07 23:16:17 +0100

    frontend index

--- barbershop/index.php
+++ barbershop/index.php
@@ -4,79 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap">
+    <link rel="stylesheet" href="src/button.css">
+    <link rel="stylesheet" href="style.css">
     <title>Holičstvo</title>
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            line-height: 1.6;
-            margin: 0;
-            padding: 0;
-            background-color: #f5f5f5;
-            color: #333;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            min-height: 100vh;
-        }
 
-        section {
-            margin: 30px;
-        }
-        header {
-            display: flex;
-            justify-content: space-around;
-            height: 100vh; /* 100% of the viewport height */
-            width: 100%;
-            background-image: url("src/barber1.png");
-            background-size: cover; /* Ensure the background image covers the entire header */
-            position: relative; /* Create a stacking context for absolute positioning */
-        }
-        .galery{
-            height: 70vh; /* 100% of the viewport height */
-            width: 100%;
-            background-image: url("src/barber1.png");
-            background-size: cover; /* Ensure the background image covers the entire header */
-            position: relative; /* Create a stacking context for absolute positioning */
-        }
-
-        section h2 {
-            font-family: 'Dancing Script', cursive;
-            font-size: 2em;
-            font-size: 50px;
-            color: white;
-            text-align: center;
-            text-shadow: 2px 2px 4px #000000;
-        }
-
-        section p {
-            font-family: 'Dancing Script', cursive;
-            font-size: 2em;
-            font-size: 30px;
-            color: white;
-            text-align: center;
-            text-shadow: 2px 2px 4px #000000;
-        }
-        .headDiv p{
-            font-family: 'Dancing Script', cursive;
-            font-size: 2em;
-            font-size: 50px;
-            color: white;
-            text-align: center;
-            text-shadow: 2px 2px 4px #000000;
-        }
-        .headDiv h1 {
-            text-align: center;
-            padding-top: 30vh;
-            font-family: 'Dancing Script', cursive;
-            font-size: 2em;
-            font-size: 80px;
-            color: white;
-            text-shadow: 2px 2px 4px #000000;
-        }
-        .formSection{
-            
-        }
     </style>
 </head>
 <script>
@@ -134,7 +66,7 @@ function getReservationsForDay(date) {
 function convertToDateFormat(inputDate) {
     const dateObject = new Date(inputDate);
     
-    // Check if the date is valid
+  
     if (isNaN(dateObject.getTime())) {
         console.error('Invalid date format');
         return null;
@@ -146,15 +78,15 @@ function convertToDateFormat(inputDate) {
 
     return `${year}-${month}-${day}`;
 }
+
 </script>
 <?php
 include 'db_connection.php';
 
-// Fetch data from the 'products' table
+
 $sql = "SELECT product_id, name,price FROM products";
 $result = $conn->query($sql);
 
-// Check if there are any results
 if ($result->num_rows > 0) {
     $products = $result->fetch_all(MYSQLI_ASSOC);
 } else {
@@ -172,56 +104,74 @@ $conn->close();
     </header>
 
 
-    <section>
+    <section style="width:100%;">
         <div class="formSection">
         <h2>Objednajte si termín</h2>
-        <p>Vyplňte nižšie uvedený formulár a objednajte si termín u nášho skúseného holiča.</p>
+        <p>Vyplňte nižšie uvedený formulár a objednajte si termín.</p>
         <form action="process_reservation.php" method="POST">
-            <label for="time">Reservation Time:</label>
+            <label for="time">Čas rezervácie:</label>
             <input type="date" id="date" name="date" min="<?php echo date('Y-m-d'); ?>" required oninput="checkWorkingDay()">
 
             <br>
-            <label for="hour">Select an hour:</label>
+            <label for="hour">Vyberte hodinu:</label>
             <select id="hour" name="hour"></select>
             <br>
-            <label for="product">Choose a product:</label>
+            <label for="product">Vyberte produkt:</label>
             <select name="product" id="product">
                 <?php foreach ($products as $product): ?>
                     <option value="<?php echo $product['name']." ".$product['price']."€"; ?>"><?php echo $product['name']." ".$product['price']."€"; ?></option>
                 <?php endforeach; ?>
             </select>
             <br>
-            <label for="firstName">First Name:</label>
+            <label for="firstName">Meno:</label>
             <input type="text" id="firstName" name="firstName" required><br>
 
-            <label for="lastName">Last Name:</label>
+            <label for="lastName">Priezvisko:</label>
             <input type="text" id="lastName" name="lastName" required><br>
 
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required><br>
 
-            <label for="phoneNumber">Phone Number:</label>
+            <label for="phoneNumber">Telefónne číslo:</label>
             <input type="tel" id="phoneNumber" name="phoneNumber" required><br>
 
-            <input type="submit" value="Submit">
+            <input type="submit" value="Odoslať">
         </form>
+
         </div>
     </section>
     <section class="galery">
+        <h2 style="text-align:left; padding-left:50px; font-size:90px;">Vyskúšajte náš e-shop</h2>
+        <div class="customButton" style="padding-left:250px;">
+            <a href="produkty.php" class="button type--A">
+                <div class="button__line"></div>
+                <div class="button__line"></div>
+                <span class="button__text">Nakupovať</span>
+                <div class="button__drow1"></div>
+                <div class="button__drow2"></div>
+            </a>
+        </div>
+        </section>
+    <section class="bottom">
+        <div>
+
+            <h2>Kontaktné informácie</h2>
+            <p>Pre otázky nás môžete kontaktovať:</p>
+            <p>Email: info@holicka.com</p>
+            <p>Telefón: +421 123 456-789</p>
+            
+            <h2>Adresa</h2>
+            <p>Navštívte nás na nasledujúcej adrese:</p>
+            <p>123 Holícka ulica, Žilina, 010 01</p>
+        </div>
+        <div >
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d774.7233349154218!2d18.746490503201738!3d49.22240398143839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssk!2ssk!4v1707342563302!5m2!1ssk!2ssk" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </section>
-    <section>
-        <h2>Kontaktné informácie</h2>
-        <p>Pre otázky nás môžete kontaktovať:</p>
-        <p>Email: info@holicka.com</p>
-        <p>Telefón: +421 123 456-789</p>
 
-        <h2>Adresa</h2>
-        <p>Navštívte nás na nasledujúcej adrese:</p>
-        <p>123 Holícka ulica, Žilina, 010 01</p>
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d162.86207600698705!2d18.739758074050453!3d49.22344859497971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssk!2ssk!4v1707149682095!5m2!1ssk!2ssk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </section>
-
-
+    <footer>
+    <p>&copy; 2024 Barber shop. All rights reserved. | Martin Ďurana</p>
+</footer>
 </body>
+
 </html>
